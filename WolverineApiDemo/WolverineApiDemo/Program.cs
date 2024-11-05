0d37c07353bdc496a7b02baa8b4d544080c42cad
Régis BOUGAUD <r.bougaud@ressources-consultants.fr>
2024-11-05 09:46:57 +0100

    update starter program

--- WolverineApiDemo/WolverineApiDemo/Program.cs
+++ WolverineApiDemo/WolverineApiDemo/Program.cs
@@ -1,7 +1,27 @@
+using FluentValidation;
+using Wolverine;
+using WolverineApiDemo.Movies.Interfaces;
+using WolverineApiDemo.Movies;
+using Wolverine.RabbitMQ;
+using MessagingContract.Requests;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Host.UseWolverine(x =>
+{
+    x.PublishAllMessages().ToRabbitExchange("movies-exc", exchange =>
+    {
+        exchange.ExchangeType = ExchangeType.Direct;
+        exchange.BindQueue("movies-queue", "exchange2movies");
+    });
+    x.UseRabbitMq(c =>
+    {
+        c.HostName = "localhost";
+    }).AutoProvision();
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<IMovieService, MovieService>();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>(ServiceLifetime.Singleton);
 
 var app = builder.Build();
 
@@ -10,12 +30,18 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
-app.UseStaticFiles();
+app.MapPost("movies", async (
+    CreateMovieRequest request, IMovieService movieService) =>
+{
+    var movie = request.MapToMovie();
+    var result = await movieService.CreateAsync(movie);
+    return Guid.TryParse(movie.Id.ToString(), out Guid movieId) ? await movieService.GetByIdAsync(movieId) : null;
+});
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
+app.MapGet("movies/{id}", async (
+    string id, IMovieService movieService) =>
+{
+    return Guid.TryParse(id, out Guid movieId) ? await movieService.GetByIdAsync(movieId) : null;
+});
+app.UseHttpsRedirection();
 app.Run();
